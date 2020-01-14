bf6fa35029fa7d6185c20cb06eb1dae2b8f3c8a3
Azlkrnr <Azlkrnr@gmail.com>
2020-01-14 14:58:04 +0800

    git push

--- resources/views/permohonan.blade.php
+++ resources/views/permohonan.blade.php
@@ -41,38 +41,83 @@
                 {{ csrf_field() }}
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="text" name="nama" class="form-control form-control-user" id="" placeholder="Nama">
+                    <input type="text" name="nama" class="form-control form-control-user" id="nama" placeholder="Nama" required autocomplete="nama" autofocus>
+                    
+                    @error('nama')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
-                  <div class="col-sm-6">
-                    <input type="text" name="jantina" class="form-control form-control-user" id="" placeholder="Jantina">
+                  
+                    <label for="jantina" class="col-md-0 col-form-label text-md-left">&nbsp&nbsp&nbspSila Pilih Jantina</label> 
+                    <div class="col-sm-4">
+                    <select id="jantina" name="jantina" class="form-control" value="{{ old('jantina') }}" required autocomplete="jantina" autofocus>
+                    
+                      <option jantina="Jantan">Lelaki</option>
+                      <option jantina="Betina">Perempuan</option>
+                  </select>
+
+                  @error('jantina')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+                    <!--<input type="text" name="jantina" class="form-control form-control-user" id="" placeholder="Jantina">-->
                   </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="text" name="no_ic" class="form-control form-control-user" id="" placeholder="No Kad Pengenalan">
-                  </div>
-                  <div class="col-sm-6">
-                    <input type="text" name="date_lahir" class="form-control form-control-user" id="" placeholder="Tarikh Lahir">
-                  </div>
-                </div>
-
-                <div class="form-group">
-                    <input type="" name="nama_kem_jab" class="form-control form-control-user" id="" placeholder="Nama Kementerian/Jabatan">
-                </div>
-
-                <div class="form-group">
-                </div>
-
-                <div class="form-group">
-                    <input type="" name="add_kem_jab" class="form-control form-control-user" id="" placeholder="Alamat Kementerian/Jabatan">
-                </div>               
-
-                <div class="form-group">
                 </div>
 
                 <div class="form-group row">
+                  <div class="col-sm-5 mb-3 mb-sm-0">
+                    <input type="text" name="no_ic" class="form-control form-control-user" id="no_ic" placeholder="No Kad Pengenalan" required autocomplete="no_ic" autofocus>
+                    @error('no_ic')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror  
+                  </div>
+                 
+                    <label for="date_lahir" class="col-md-0 col-form-label text-md-left">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspTarikh Lahir &nbsp&nbsp</label> 
+                    <input type="date" name="date_lahir" class="form-control form-control-user col-md-4" id="date_lahir" placeholder="Tarikh Lahir" required autocomplete="date_lahir" autofocus>
+                    @error('date_lahir')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <input type="" name="nama_kem_jab" class="form-control form-control-user" id="nama_kem_jab" placeholder="Nama Kementerian/Jabatan" required autocomplete="nama_kem_jab" autofocus>
+                    @error('nama_kem_jab')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>
+
+                <div class="form-group">
+                </div>
+
+                <div class="form-group">
+                    <input type="" name="add_kem_jab" class="form-control form-control-user" id="add_kem_jab" placeholder="Alamat Kementerian/Jabatan" required autocomplete="add_kem_jab" autofocus>
+                    @error('add_kem_jab')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                  </div>               
+
+                <div class="form-group">
+                </div>
+
+                <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="email" name="email" class="form-control form-control-user" id="" placeholder="Alamat Email">                    
+                    <input type="email" name="email" class="form-control form-control-user" id="email" placeholder="Alamat Email" required autocomplete="email" autofocus>                    
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
                   <div class="col-sm-6">
 
@@ -81,44 +126,111 @@
 
                <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="" name="no_tel" class="form-control form-control-user" id="" placeholder="Nombor Telefon">
+                    <input type="" name="no_tel" class="form-control form-control-user" id="no_tel" placeholder="Nombor Telefon" required autocomplete="no_tel" autofocus>
+                    @error('no_tel')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
+
                   <div class="col-sm-6">
-                    <input type="" name="no_fax" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Nombor Fax">
+                    <input type="" name="no_fax" class="form-control form-control-user" id="no_fax" placeholder="Nombor Fax" required autocomplete="no_fax" autofocus>
+                    @error('no_fax')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
                 </div>
 
                <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="" name="no_ph" class="form-control form-control-user" id="" placeholder="Nombor Handphone">
+                    <input type="" name="no_ph" class="form-control form-control-user" id="no_ph" placeholder="Nombor Handphone" required autocomplete="no_ph" autofocus>
+                    @error('no_ph')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
+
                   <div class="col-sm-6">
-                    <input type="" name="jawatan" class="form-control form-control-user" id="" placeholder="Jawatan">
+                    <input type="" name="jawatan" class="form-control form-control-user" id="jawatan" placeholder="Jawatan" required autocomplete="jawatan" autofocus>
+                    @error('jawatan')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
                 </div>   
 
                <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="" name="gred" class="form-control form-control-user" id="" placeholder="Gred">
+                    <input type="" name="gred" class="form-control form-control-user" id="gred" placeholder="Gred" required autocomplete="gred" autofocus>
+                    @error('gred')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
+
                   <div class="col-sm-6">
-                    <input type="" name="bil_tahun" class="form-control form-control-user" id="" placeholder="Bilangan Tahun Berkhidmat">
+                    <input type="" name="bil_tahun" class="form-control form-control-user" id="bil_tahun" placeholder="Bilangan Tahun Berkhidmat" required autocomplete="bil_tahun" autofocus>
+                    @error('bil_tahun')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
                 </div>   
 
                <div class="form-group row">
-                  <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="" name="date_sara" class="form-control form-control-user" id="" placeholder="Tarikh Wajib Bersara">
-                  </div>
+               
+                    
+                    <label for="date_sara" class="col-md-0 col-form-label text-md-left">&nbsp&nbsp&nbspTarikh Wajib Bersara &nbsp&nbsp</label> 
+                    <input type="date" name="date_sara" class="form-control form-control-user col-md-4" id="date_sara" placeholder="Tarikh Wajib Bersara" required autocomplete="date_sara" autofocus>
+                    
+                   <!-- <input type="date" name="date_sara" class="form-control form-control-user" id="date_sara" placeholder="Tarikh Wajib Bersara" required autocomplete="date_sara" autofocus>-->
+                    @error('date_sara')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                
+
                   <div class="col-sm-6">
-                    <input type="" name="bil_sess" class="form-control form-control-user" id="" placeholder="Bilangan Sesi Terdahulu">
+                    <input type="" name="bil_sess" class="form-control form-control-user" id="bil_sess" placeholder="Bilangan Sesi Terdahulu" required autocomplete="bil_sess" autofocus>
+                    @error('bill_sess')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                   </div>
-                </div>                            
+                </div>            
+                
+                <div class="form-group row">
+                  <div class="col-md-6">
+                      <input id="password" placeholder="Kata Laluan" type="password" class="form-control form-control-user col-md-0 col-form-label text-md-left" name="password" required autocomplete="new-password">
+                      @error('password')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                  </div>
+                </div>
+                    
+                <div class="form-group row">
+                
+                        <div class="col-md-6">
+                          <input id="password-confirm" placeholder="Pastikan Kata Laluan" type="password" class="form-control form-control-user col-md-0 col-form-label text-md-left" name="password_confirmation" required autocomplete="new-password">
+                        </div>
+                </div>
+              </div>          
 
                 <!--<a href="login.html" class="btn btn-primary btn-user btn-block">
                   Hantar Permohonan
                 </a>-->
-                <div class="text-right">
+                <div class="text-center">
                     <button type="submit" class="btn btn-info mt-4">{{ __('Hantar Permohonan') }}</button>
                 </div>          
                 <!--<hr>-->
